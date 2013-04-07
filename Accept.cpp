9a43c6a54543af8ef41f8fba561dac11a8f0f00a
caronnee <caronnee@79bdd746-0bb5-be63-c506-d8b663197dbc>
2013-04-07 10:45:57 +0000

    Regular expression work for dot mark

--- Accept.cpp
+++ Accept.cpp
@@ -4,7 +4,6 @@
 
 State::State() : _size(0) {}
 
-
 State * State::Next() { return NULL; }
 
 void State::SetNext( State * state ) {}
--- AcceptAny.cpp
+++ AcceptAny.cpp
@@ -1,1 +1,11 @@
 #include "stdafx.h"
+#include "AcceptAny.h"
+
+StateAnyChar::StateAnyChar(){}
+
+bool StateAnyChar::Accept(char ch, SearchStateArray & cands)
+{
+	return State::Accept(cands,Next());
+}
+
+StateAnyChar::~StateAnyChar() { }
